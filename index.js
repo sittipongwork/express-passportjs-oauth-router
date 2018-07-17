1ccd62e71251d3c40e564e7001b04a2bbe36498a
Sittipong Jungsakul <sittipongwork@gmail.com>
2018-07-17 13:49:56 +0700

    move facebook login router to new oauth

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 var express = require('express')
 var router = express.Router()
-
 var passport = require('passport')
-var FacebookStrategy = require('passport-facebook').Strategy;
 
 router.use(require('cookie-parser')());
 router.use(require('body-parser').urlencoded({ extended: true }));
@@ -11,6 +9,9 @@ router.use(require('express-session')({
   resave: true,
   saveUninitialized: true
 }));
+
+var facebookAuthRouter = require('./facebookAuth')
+
 router.use(passport.initialize());
 router.use(passport.session());
 
@@ -19,37 +20,6 @@ router.get('/', function (req, res) {
   res.send('Auth index home page')
 })
 
-// =============== Facebook Login Strategy ==============
-
-passport.use(new FacebookStrategy({
-    clientID: process.env.FACEBOOK_KEY,
-    clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: "http://localhost:8080/auth/facebook/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // Query User whatever you want
-    User.findOrCreate(..., function(err, user) {
-        if (err) { return done(err); }
-        done(null, user);
-    });
-  }
-));
-
-passport.serializeUser(function(user, done) {
-    done(null, user);
-});
-
-passport.deserializeUser(function(user, done) {
-    done(null, user);
-});
-
-// GET Login by facebook page
-router.get('/facebook', passport.authenticate('facebook'));
-//router.get('/facebook', passport.authenticate('facebook'), { scope: 'read_stream' });
-
-// GET Login by facebook callback
-router.get('/facebook/callback',
-  passport.authenticate('facebook', { successRedirect: '/auth',
-                                      failureRedirect: '/' }));
+router.use('/facebook', facebookAuthRouter)
 
 module.exports = router
